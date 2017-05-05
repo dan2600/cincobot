df94e653a490b6b3ed4d000ef1e8a9e1dd809062
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-05-05 14:24:55 -0400

    updated

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $postback = $input['entry'][0]['messaging'][0]['postback']['payload'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
 $userinfo = json_decode(file_get_contents('https://graph.facebook.com/v2.6/'.$sender.'?access_token='.$access_token), true);
-error_log(print_r($userinfo, true));
+
 
 
 
@@ -55,6 +55,8 @@ else
   }
 }';
 }
+
+error_log(print_r($jsonData, true));
 
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 $ch = curl_init($url);
